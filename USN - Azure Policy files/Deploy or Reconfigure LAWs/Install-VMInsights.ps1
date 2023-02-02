f609325b8970d1215afe180bfc80c424ac1f3396
PGILL-1973 <123421245+PGILL-1973@users.noreply.github.com>
2023-02-02 18:41:35 -0500

    Add files via upload

---
+++ USN - Azure Policy files/Deploy or Reconfigure LAWs/Install-VMInsights.ps1
@@ -0,0 +1,97 @@
+Get-Help .\Install-VMInsights.ps1 -Detailed
+
+SYNOPSIS
+    This script installs VM extensions for Log Analytics and the Dependency agent as needed for VM Insights.
+
+
+SYNTAX
+    .\Install-VMInsights.ps1 [-WorkspaceId] <String> [-WorkspaceKey] <String> [-SubscriptionId] <String> [[-ResourceGroup]
+    <String>] [[-Name] <String>] [[-PolicyAssignmentName] <String>] [-ReInstall] [-TriggerVmssManualVMUpdate] [-Approve] [-WorkspaceRegion] <String>
+    [-WhatIf] [-Confirm] [<CommonParameters>]
+
+
+DESCRIPTION
+    This script installs or reconfigures the following on VMs and virtual machine scale sets:
+    - Log Analytics VM extension configured to supplied Log Analytics workspace
+    - Dependency agent VM extension
+
+    Can be applied to:
+    - Subscription
+    - Resource group in a subscription
+    - Specific VM or virtual machine scale set
+    - Compliance results of a policy for a VM or VM extension
+
+    Script will show you a list of VMs or virtual machine scale sets that will apply to and let you confirm to continue.
+    Use -Approve switch to run without prompting, if all required parameters are provided.
+
+    If the extensions are already installed, they will not install again.
+    Use -ReInstall switch if you need to, for example, update the workspace.
+
+    Use -WhatIf if you want to see what would happen in terms of installs, what workspace configured to, and status of the extension.
+
+
+PARAMETERS
+    -WorkspaceId <String>
+        Log Analytics WorkspaceID (GUID) for the data to be sent to
+
+    -WorkspaceKey <String>
+        Log Analytics Workspace primary or secondary key
+
+    -SubscriptionId <String>
+        SubscriptionId for the VMs/VM Scale Sets
+        If using PolicyAssignmentName parameter, subscription that VMs are in
+
+    -ResourceGroup <String>
+        <Optional> Resource Group to which the VMs or VM Scale Sets belong
+
+    -Name <String>
+        <Optional> To install to a single VM/VM Scale Set
+
+    -PolicyAssignmentName <String>
+        <Optional> Take the input VMs to operate on as the Compliance results from this Assignment
+        If specified will only take from this source.
+
+    -ReInstall [<SwitchParameter>]
+        <Optional> If VM/VM Scale Set is already configured for a different workspace, set this to change to the new workspace
+
+    -TriggerVmssManualVMUpdate [<SwitchParameter>]
+        <Optional> Set this flag to trigger update of VM instances in a scale set whose upgrade policy is set to Manual
+
+    -Approve [<SwitchParameter>]
+        <Optional> Gives the approval for the installation to start with no confirmation prompt for the listed VMs/VM Scale Sets
+
+    -WorkspaceRegion <String>
+        Region the Log Analytics Workspace is in
+        Supported values: "East US","eastus","Southeast Asia","southeastasia","West Central US","westcentralus","West Europe","westeurope"
+        For Health supported is: "East US","eastus","West Central US","westcentralus"
+
+    -WhatIf [<SwitchParameter>]
+        <Optional> See what would happen in terms of installs.
+        If extension is already installed will show what workspace is currently configured, and status of the VM extension
+
+    -Confirm [<SwitchParameter>]
+        <Optional> Confirm every action
+
+    <CommonParameters>
+        This cmdlet supports the common parameters: Verbose, Debug,
+        ErrorAction, ErrorVariable, WarningAction, WarningVariable,
+        OutBuffer, PipelineVariable, and OutVariable. For more information, see
+        about_CommonParameters (https://go.microsoft.com/fwlink/?LinkID=113216).
+
+    -------------------------- EXAMPLE 1 --------------------------
+    .\Install-VMInsights.ps1 -WorkspaceRegion eastus -WorkspaceId <WorkspaceId> -WorkspaceKey <WorkspaceKey> -SubscriptionId <SubscriptionId>
+    -ResourceGroup <ResourceGroup>
+
+    Install for all VMs in a resource group in a subscription
+
+    -------------------------- EXAMPLE 2 --------------------------
+    .\Install-VMInsights.ps1 -WorkspaceRegion eastus -WorkspaceId <WorkspaceId> -WorkspaceKey <WorkspaceKey> -SubscriptionId <SubscriptionId>
+    -ResourceGroup <ResourceGroup> -ReInstall
+
+    Specify to reinstall extensions even if already installed, for example, to update to a different workspace
+
+    -------------------------- EXAMPLE 3 --------------------------
+    .\Install-VMInsights.ps1 -WorkspaceRegion eastus -WorkspaceId <WorkspaceId> -WorkspaceKey <WorkspaceKey> -SubscriptionId <SubscriptionId>
+    -PolicyAssignmentName a4f79f8ce891455198c08736 -ReInstall
+
+    Specify to use a PolicyAssignmentName for source and to reinstall (move to a new workspace)
